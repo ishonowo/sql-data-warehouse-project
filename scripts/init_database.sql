39e193d8a99d5c8c69a8d7142e4df6f019304f42
ishonowo <ikenna.shonowo4@gmail.com>
2026-02-26 23:33:01 +0100

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,6 +1,12 @@
+/*
+This is the initial scripts for creating the database called DataWarehouse and its schemas.
+*/
+--Create Database 
 CREATE DATABASE DataWarehouse;
 USE DataWarehouse;
 GO;
+
+--Create schemas
 CREATE SCHEMA bronze;
 GO;
 CREATE SCHEMA silver;
